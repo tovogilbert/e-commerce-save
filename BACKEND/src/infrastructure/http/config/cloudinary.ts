89d6f832282ded9acff9e9336e00790766d48835
tovogilbert <tovogilbert283@gmail.com>
2025-06-11 16:27:18 +0300

    Ajout du fichier manquan pour le middlware et la configuration cloudinary

---
+++ BACKEND/src/infrastructure/http/config/cloudinary.ts
@@ -0,0 +1,10 @@
+// infrastructure/cloudinary.ts
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: 'dgmbjda9t',
+  api_key: '267912725349718',
+  api_secret: 'eZNP8rZxHtUl3KOgGKn5mNXhXGM'
+});
+
+export default cloudinary;
