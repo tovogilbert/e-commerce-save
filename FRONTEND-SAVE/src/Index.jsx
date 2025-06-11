7fc1b8cc2ee543e9f59b0bec2ad291a48c52de26
tovogilbert <tovogilbert283@gmail.com>
2025-06-11 20:18:29 +0300

    Premier commit pour le developpement frontend

---
+++ FRONTEND-SAVE/src/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { CartProvider } from './contexts/CartContext';
+import CartModal from './components/CartModal';
+
+
+const Index = () => {
+  return (
+    <div>
+      Main
+    </div>
+  );
+};
+
+export default Index;
