d46a2d2ec11cc646ce656b3319ca2e785e668726
tovogilbert <tovogilbert283@gmail.com>
2025-06-11 16:53:53 +0300

    Modification de la réponse du dto client et le mapper payement

--- BACKEND/src/infrastructure/http/mappers/PaymentMapper.ts
+++ BACKEND/src/infrastructure/http/mappers/PaymentMapper.ts
@@ -3,16 +3,16 @@ import { PaymentResponseDTO } from "../dtos/responses/payment.dto";
 
 export class PaymentMapper {
   static toDTO(payment: Payment): PaymentResponseDTO {
-    return {
-      "id": payment.id,
-      "paymentDate": payment.paymentDate,
-      "amount": payment.amount,
-      "method": payment.method,
-      "transactionRef": payment.transactionRef,
-      "createdAt": new Date(), 
-      "updatedAt": new Date() 
-    };
-  }
+  return {
+    id: payment.id,
+    paymentDate: payment.paymentDate,
+    amount: payment.amount,
+    method: payment.method,
+    transactionRef: payment.transactionRef,
+    createdAt: payment.paymentDate, // À remplacer si vous avez accès aux vrais champs
+    updatedAt: payment.paymentDate
+  };
+}
 
   static toDTOList(payments: Payment[]): PaymentResponseDTO[] {
     return payments.map(payment => this.toDTO(payment));
