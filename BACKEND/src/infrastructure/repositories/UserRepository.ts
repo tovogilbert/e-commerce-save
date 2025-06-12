ae4370bf5c18412b21f98de551d7a905a26ff430
chistinah <christinah441@gmail.com>
2025-06-12 20:36:29 +0300

    Repository d'implémentation pour l'utilisateur

---
+++ BACKEND/src/infrastructure/repositories/UserRepository.ts
@@ -0,0 +1,62 @@
+import { User } from "../../core/entities/user/User";
+import { IUserRepository } from "../../interfaces/repositories/IUserRepository";
+import { UserModel } from "../db/models/userModel";
+import { hashPassword } from "../../shared/utils/auth";
+
+export class UserRepository implements IUserRepository {
+  async create(user: Omit<User, "id">): Promise<User> {
+    const hashedPassword = await hashPassword(user.password);
+    const createdUser = await UserModel.create({
+      ...user,
+      password: hashedPassword
+    });
+    
+    return this.toDomainEntity(createdUser);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await UserModel.findOne({ where: { email } });
+    return user ? this.toDomainEntity(user) : null;
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = await UserModel.findByPk(id);
+    return user ? this.toDomainEntity(user) : null;
+  }
+
+  async update(user: User): Promise<User> {
+    const [_, [updatedUser]] = await UserModel.update(
+      {
+        name: user.name,
+        email: user.email,
+        password: user.password
+      },
+      {
+        where: { id: user.id },
+        returning: true
+      }
+    );
+    
+    return this.toDomainEntity(updatedUser);
+  }
+
+  async findAll(): Promise<User[]> {
+    const users = await UserModel.findAll();
+    return users.map(this.toDomainEntity);
+  }
+
+  async delete(id: string): Promise<void> {
+    await UserModel.destroy({ where: { id } });
+  }
+
+  private toDomainEntity(userModel: UserModel): User {
+    return new User(
+      userModel.id,
+      userModel.name,
+      userModel.email,
+      userModel.password,
+      userModel.createdAt,
+      userModel.updatedAt
+    );
+  }
+}
